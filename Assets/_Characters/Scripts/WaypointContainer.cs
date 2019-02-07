9fde8445b08353047304136c07fce5aac3692d54
Felipe Del Canto <felipe.delcanto@icloud.com>
2019-02-07 17:01:49 -0300

    Lecture 166: Created Patrol Path GameObject

---
+++ Assets/_Characters/Scripts/WaypointContainer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Characters
+{
+    public class WaypointContainer : MonoBehaviour
+    {
+        void OnDrawGizmos()
+        {
+            Vector3 firstPosition = transform.GetChild(0).position;
+            Vector3 previousPosition = firstPosition; 
+
+            foreach (Transform waypoint in transform)
+            {
+                Gizmos.color = Color.blue;
+                Gizmos.DrawSphere(waypoint.position, 0.2f);
+
+                Gizmos.color = Color.red;
+                Gizmos.DrawLine(waypoint.position, previousPosition);
+                previousPosition = waypoint.position;
+            }
+            Gizmos.DrawLine(previousPosition, firstPosition);
+        }
+    }
+}
